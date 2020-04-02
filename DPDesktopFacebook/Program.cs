76a1bd63f28795ecef85c671a58e9084e0d9951f
Avital Rush <avital29394@gmail.com>
2020-04-02 19:14:51 +0300

    comment hello

--- DPDesktopFacebook/Program.cs
+++ DPDesktopFacebook/Program.cs
@@ -13,6 +13,7 @@ namespace DPDesktopFacebook
           [STAThread]
           static void Main()
           {
+            // hello
                Application.EnableVisualStyles();
                Application.SetCompatibleTextRenderingDefault(false);
                Application.Run(new Form1());
